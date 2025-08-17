89ac131cea3ddcc3844280e353b968488ba1b976
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-08-18 05:42:52 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginRegPage from "./components/LoginRegPage"; // Import the new combined AuthPage
 import MainUI from "./components/MainUI";
+import ProfilePage from "./components/ProfilePage"
+import BecomeVendorPage from "./components/BecomeVendorPage";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path="/login" element={<LoginRegPage />} />
 
         <Route path="/main" element={<MainUI />} />
+
+        <Route path="/profile" element={< ProfilePage />} />
+        <Route path="/becomeVendor" element={< BecomeVendorPage />} />
       </Routes>
     </Router>
   );
