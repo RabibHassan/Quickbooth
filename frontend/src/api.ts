b394043f2efd4856175a3e43ea29c87dda38cec9
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-08-18 05:44:10 +0600

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -23,3 +23,29 @@ export const logout = () => {
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
 };
+
+//View profile
+
+export const getProfile = () => {
+  const token = localStorage.getItem("access_token");
+  return axios.get(`${API_URL}/view_profile/`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+};
+
+export const updateProfile = async (
+  id: string,
+  username: string,
+  email: string,
+  password: string
+) => {
+  return axios.put(`${API_URL}/update/`, { id, username, email, password })
+}
+
+export const vendorForm = async (
+  store_name: string
+) => {
+  return axios.post(`${API_URL}/vendor_form/`, { store_name })
+}
