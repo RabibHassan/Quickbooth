587c50e8efdcbfc4735574b958247e0a93b8c7ed
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-08-18 05:42:01 +0600

    Update urls.py

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('register/',views.register_user,name='register'),
     path('login/',views.login_user,name='login'),
     path('logout/',views.logout_user,name='logout'),
+    path('update/',views.update_user,name='update'),
+    path('view_profile/',views.view_profile,name="view_profile"),
+    path('vendor_form/',views.vendor_form,name="vendor_form")
 ]
