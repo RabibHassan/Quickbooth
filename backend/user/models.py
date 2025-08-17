16d5d716c0d41ff46ee395a024b9bdacc98d4ad6
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-08-18 05:41:31 +0600

    Update models.py

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+# Create your models here.from django.db import models
+from datetime import timedelta
+from django.utils import timezone
+
 # Create your models here.
+class Vendor(models.Model):
+    store_id = models.AutoField(primary_key=True)
+    store_name = models.CharField(max_length=255, unique=True)
+    total_products_online = models.BigIntegerField(default=0)
+    subscription_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.store_name} - {self.store_id}"
+
