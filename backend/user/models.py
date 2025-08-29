7a1911f5a535309fafc24057073b193c18f33a84
RabibHassan <rababechan@gmail.com>
2025-08-30 00:20:38 +0600

    comm

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from datetime import timedelta
 from django.utils import timezone
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
 # Create your models here.
 class Vendor(models.Model):
     vendor_id=models.BigIntegerField(null=True)
