5c8fec73d47c170de8ec6cd16e61e72c8f2d309b
RabibHassan <rababechan@gmail.com>
2025-08-30 01:51:33 +0600

    Bismillah

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,2 @@
-// For production
 const API_URL = "https://quickbooth-2.onrender.com/api/auth";
-
-// For development (if needed)
-// const API_URL = "http://localhost:8000/api/auth";
-
 export default API_URL;
